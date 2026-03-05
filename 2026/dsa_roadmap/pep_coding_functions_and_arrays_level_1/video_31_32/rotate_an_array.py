b5e12243726432f68f17dc86c969bf3de951b9d6
rutvikshah <rutvikjshah@gmail.com>
2026-03-05 11:13:18 +0530

    PEPCODING-VIDEO-31-32: Rotate An Array

--- 2026/dsa_roadmap/pep_coding_functions_and_arrays_level_1/video_31_32/rotate_an_array.py
+++ 2026/dsa_roadmap/pep_coding_functions_and_arrays_level_1/video_31_32/rotate_an_array.py
@@ -26,3 +26,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""CLI Output
+(basevenv) rutvikshah@Rutviks-MacBook-Air video_31_32 % python3 rotate_an_array.py                            
+Input array:
+['a', 'b', 'c', 'd', 'e']
+
+Number of rotations we want to perform: 6
+Array after rotation: 1 is ['e', 'a', 'b', 'c', 'd']
+Array after rotation: 2 is ['d', 'e', 'a', 'b', 'c']
+Array after rotation: 3 is ['c', 'd', 'e', 'a', 'b']
+Array after rotation: 4 is ['b', 'c', 'd', 'e', 'a']
+Array after rotation: 5 is ['a', 'b', 'c', 'd', 'e']
+Array after rotation: 6 is ['e', 'a', 'b', 'c', 'd']
+
+Final array after 6 rotation
+['e', 'a', 'b', 'c', 'd']
+"""
