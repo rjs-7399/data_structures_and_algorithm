93206178e9e66bf651e0a43f725d9da36d96f2c0
rutvikshah <rutvikjshah@gmail.com>
2026-03-13 09:01:49 +0530

    PEPCODING-VIDEO-33-34: Inverse Of An Array

--- 2026/dsa_roadmap/pep_coding_functions_and_arrays_level_1/video_33_34/inverse_of_an_array.py
+++ 2026/dsa_roadmap/pep_coding_functions_and_arrays_level_1/video_33_34/inverse_of_an_array.py
@@ -22,3 +22,19 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""CLI Output
+(basevenv) rutvikshah@Rutviks-MacBook-Air video_33_34 % python3 inverse_of_an_array.py 
+Input Array:
+[3, 4, 1, 2, 0]
+
+Printing Buffer Dictionary:
+Printing buffer at index 0: {3: 0}
+Printing buffer at index 1: {3: 0, 4: 1}
+Printing buffer at index 2: {3: 0, 4: 1, 1: 2}
+Printing buffer at index 3: {3: 0, 4: 1, 1: 2, 2: 3}
+Printing buffer at index 4: {3: 0, 4: 1, 1: 2, 2: 3, 0: 4}
+
+Output Array after inverse operation:
+[4, 2, 3, 0, 1]
+"""
